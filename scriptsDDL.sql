c046d1fb2886208f9872418c9faf9b179f2f8da3
SthefanyClaros <smclarosv@gmail.com>
2025-11-14 10:14:45 -0600

    Añadiendo entidad Categoria

--- scriptsDDL.sql
+++ scriptsDDL.sql
@@ -8,30 +8,26 @@ CREATE TABLE ventas_directas.usuarios (
     fecha_registro DATETIME DEFAULT SYSDATETIME()
 )
 
-CREATE TABLE ventas_directas.productos
-(
+CREATE TABLE ventas_directas.productos (
     id INT IDENTITY (1,1) PRIMARY KEY,
     nombre_producto NVARCHAR (150) NOT NULL,
     descripcion_producto NVARCHAR (300),
     precio_unitario FLOAT NOT NULL,
     activo BIT DEFAULT 1,
-    categoria NVARCHAR (100) 
+    id_categoria INT NOT NULL
 )
 
-CREATE TABLE ventas_directas.stock
-(
+CREATE TABLE ventas_directas.stock (
     id INT IDENTITY (1,1) PRIMARY KEY,
     id_productos INT NOT NULL,
     cantidad_disponible INT DEFAULT 0,
-    ubicacion NVARCHAR (100)
 )
 
 ALTER TABLE ventas_directas.stock
 ADD CONSTRAINT FK_Catalogo_Stock
 FOREIGN KEY (id_productos) REFERENCES ventas_directas.productos(id)
 
-CREATE TABLE ventas_directas.Orden_pedido
-(
+CREATE TABLE ventas_directas.Orden_pedido (
     id INT IDENTITY (1,1) PRIMARY KEY,
     id_usuarios INT NOT NULL,
     fecha_orden DATETIME DEFAULT SYSDATETIME(),
@@ -45,14 +41,14 @@ ALTER TABLE ventas_directas.Orden_pedido
 ADD CONSTRAINT FK_Cliente_OrdenPedido
 FOREIGN KEY (id_usuarios) REFERENCES ventas_directas.usuarios(id)
 
-CREATE TABLE ventas_directas.Detalle_pedido
-(
+CREATE TABLE ventas_directas.Detalle_pedido (
     id INT IDENTITY (1,1) PRIMARY KEY,
     id_orden INT NOT NULL,
     id_productos INT NOT NULL,
     cantidad INT NOT NULL,
     precio_unitario FLOAT NOT NULL,
     subtotal AS (cantidad * precio_unitario) PERSISTED,
+
 )
 
 ALTER TABLE ventas_directas.Detalle_pedido
@@ -62,5 +58,14 @@ FOREIGN KEY (id_orden) REFERENCES ventas_directas.Orden_pedido(id)
 ALTER TABLE ventas_directas.Detalle_pedido
 ADD CONSTRAINT FK_Catalogo_DetallePedido
 FOREIGN KEY (id_productos) REFERENCES ventas_directas.productos(id)
+
+CREATE TABLE ventas_directas.Categoria (
+    id INT IDENTITY (1,1) PRIMARY KEY,
+    nombre_categoria NVARCHAR (100) NOT NULL
+)
+
+ALTER TABLE ventas_directas.productos
+ADD CONSTRAINT FK_Productos_Categoria
+FOREIGN KEY (id_categoria) REFERENCES ventas_directas.Categoria(id)
 
 
