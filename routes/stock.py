dc71506b056e8ede9967738b11b5bfad274e605b
SthefanyClaros <smclarosv@gmail.com>
2025-11-16 23:23:34 -0600

    Actualizacion de routes de stock

--- routes/stock.py
+++ routes/stock.py
@@ -1,33 +1,37 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, Request, status
 from models.stock import Stock
-
-from controllers.categoria import (
-    get_one_producto,
-    get_all_productos,
-    create_producto,
-    update_producto,
-    delete_producto
+from controllers.stock import (
+    get_all_stocks,
+    get_one_stck,
+    create_stock,
+    delete_stock,
+    update_stock
 )
 
-router = APIRouter(prefix="/productos")
+router = APIRouter(prefix = "/stock")
 
-@router.get("/{id}", tags=["Productos"], status_code=status.HTTP_200_OK)
-async def get_one_categoria_endpoint(id: int):
-    result: Productos = await get_one_producto(id)
+@router.get( "/" , tags=["Stock"])
+async def get_products_stock():
+    result = await get_all_stocks()
     return result
 
-@router.get("/", tags=["Productos"], status_code=status.HTTP_200_OK)
-async def get_all_productos_endpoint():
-    result = await get_all_productos()
+@router.get("/{id}", tags=["Stock"])
+async def get_one_stock( id: int ):
+    result: Stock =  await get_one_stck(id)
     return result
 
-@router.post("/", tags=["Productos"], status_code=status.HTTP_201_CREATED)
-async def create_new_producto(productos_data: Productos):
-    result = await create_producto(productos_data)
+@router.post( "/" , tags = ["Stock"])
+async def create_new_stock(stock_data: Stock):
+    result = await create_stock(stock_data)
     return result
 
-@router.put("/{id}", tags=["Producto"], status_code=status.HTTP_200_OK)
-async def update_producto_information(id: int, producto_data: Productos):
-    producto_data.id = id
-    result = await update_producto(producto_data)
+@router.put("/{id}", tags=["Stock"])
+async def update_stock_information( stock_data: Stock , id: int ):
+    stock_data.id = id
+    result = await update_stock(stock_data)
     return result
+
+@router.delete("/{id}", tags=["Stock"], status_code=status.HTTP_204_NO_CONTENT)
+async def delete_product_stock( id: int ):
+    status: str =  await delete_stock(id)
+    return status
