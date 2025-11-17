a5e5a1739767da00621e47ca232dd96100984a3e
SthefanyClaros <smclarosv@gmail.com>
2025-11-16 23:19:54 -0600

    Actualizacion del main

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import uvicorn 
 from fastapi import FastAPI
 from routes.usuarios import router as router_usuarios
+from routes.categoria import router as router_categoria
+from routes.productos import router as router_productos
+from routes.stock import router as router_stock
 
 app = FastAPI()
 
 app.include_router(router_usuarios)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Customer!"}
+app.include_router(router_categoria)
 
+app.include_router(router_productos)
+
+app.include_router(router_stock)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
-    
-from routes.usuarios import router as router_usuarios
-app.include_router(router_usuarios)
