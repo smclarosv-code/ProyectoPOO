aac4cb9139a81e5710d692b73ee8ab9080dbfd12
SthefanyClaros <smclarosv@gmail.com>
2025-11-16 23:23:53 -0600

    Actualizacion de routes de usuarios

--- routes/usuarios.py
+++ routes/usuarios.py
@@ -1,12 +1,47 @@
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter
+from fastapi.concurrency import run_in_threadpool
+from models.usuarios import Usuarios
+from controllers.usuarios import create_user
+
+router = APIRouter(prefix="/usuarios")
+
+@router.post("/", tags=["Usuarios"])
+async def create_new_user(usuario_data: Usuarios):
+    return await run_in_threadpool(create_user, usuario_data)
+from fastapi import APIRouter, HTTPException, Request, status
 from models.usuarios import Usuarios
 from controllers.usuarios import (
-    create_user
+    create_user,
+    update_user,
+    get_all_u,
+    get_one_u,
+    delete_user
 )
 
 router = APIRouter(prefix = "/usuarios")
+
+@router.get( "/" , tags=["Usuarios"])
+async def get_all_users():
+    result = await get_all_u()
+    return result
+
+@router.get("/{id}", tags=["Usuarios"])
+async def get_one_user( id: int ):
+    result: Usuarios =  await get_one_u(id)
+    return result
 
 @router.post( "/" , tags = ["Usuarios"])
 async def create_new_user(usuario_data: Usuarios):
     result = await create_user(usuario_data)
     return result
+
+@router.put("/{id}", tags=["Usuarios"])
+async def update_user_information( usuario_data: Usuarios , id: int ):
+    usuario_data.id = id
+    result = await update_user(usuario_data)
+    return result
+
+@router.delete("/{id}", tags=["Usuarios"], status_code=status.HTTP_204_NO_CONTENT)
+async def delete_existing_user( id: int ):
+    status: str =  await delete_user(id)
+    return status
