96a9bd54aee81985472b9fd90cfd3a11ddb2e511
SthefanyClaros <smclarosv@gmail.com>
2025-11-16 23:21:14 -0600

    Actualizacion de modelos de productos

--- models/productos.py
+++ models/productos.py
@@ -8,28 +8,35 @@ class Productos(BaseModel):
         description="ID del producto"
     )
 
-    nombre_producto: str = Field(
+    nombre_producto: Optional[str] = Field(
         description="Nombre del producto",
         max_length=150,
-        pattern=r"^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ0-9' ()-]+$"
+        pattern=r"^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ0-9' ()-]+$",
+        examples=["Optimus Lima-Limon", "Base de Maquillaje"],
+        default = None
     )
 
     descripcion_producto: Optional[str] = Field(
         description="Descripción del producto",
         max_length=300,
         pattern=r"^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ0-9.,' ()-]+$",
+        examples=["Suplemento vitamínico sabor lima-limón", "Base líquida para maquillaje de larga duración"],
         default=None
     )
 
-    precio_unitario: float = Field(
-        description="Precio unitario del producto"
+    precio_unitario: Optional[float] = Field(
+        description="Precio unitario del producto",
+        examples=[1000.00, 975.00],
+        default = None
     )
 
     activo: Optional[bool] = Field(
         default=True,
-        description="Estado activo"
+        description="Estado activo",
+        examples=[True, False]
     )
 
-    id_categoria: int = Field(
-        description="ID de categoría (FK)"
+    id_categoria: Optional[int] = Field(
+        description="ID de categoría (FK)",
+        default = None
     )
