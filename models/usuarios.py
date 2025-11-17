3ad208810b042ad6290b2518b89fa965cfc744a9
SthefanyClaros <smclarosv@gmail.com>
2025-11-16 23:22:02 -0600

    Actualizacion de modelos de usuarios

--- models/usuarios.py
+++ models/usuarios.py
@@ -1,3 +1,5 @@
+# models se definen las clases que están alineadas a las traducciones del modelo de datos
+
 from pydantic import BaseModel, Field, field_validator
 from typing import Optional
 import re
@@ -12,19 +14,22 @@ class Usuarios(BaseModel):
     nombre_completo: str = Field(
         description="Nombre completo del Usuario",
         pattern=r"^[A-Za-zÁÉÍÓÚÜÑáéíóúüñ' -]+$",
-        examples=["Eduardo Enrique Ponce Palma"]
+        examples=["Eduardo Enrique Ponce Palma"],
+        default=None
     )
 
     telefono: Optional[str] = Field(
         description="Telefono del Usuario",
         pattern=r"^[0-9 +()-]{7,20}$",
-        examples=["98765432"]
+        examples=["98765432"],
+        default=None
     )
 
     correo: Optional[str] = Field(
         description="Correo electrónico del usuario",
         pattern=r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
-        examples=["eduardo@example.com"]
+        examples=["eduardo@example.com"],
+        default=None
     )
 
     fecha_registro: Optional[datetime] = Field(
